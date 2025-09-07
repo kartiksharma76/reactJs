e50ee274ff4f3ab6844ec1f911132b1223b9383f
kartik <kartiksharma768976@gmail.com>
2025-09-07 17:35:00 +0530

    react.added

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>hello kartik | basic study  and setup react </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+  <h1>kartik with vite | KS</h1>
+  )
+}
+
+export default App
