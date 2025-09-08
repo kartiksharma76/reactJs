ae37aaf96ef5a0859c14d3eab8d9d4302f7edf23
kartik <kartiksharma768976@gmail.com>
2025-09-09 00:11:30 +0530

    react.added

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
-import Chai from './chai';  
+import Chai from './chai'; 
+ 
 function App() {
-
+const username = "Kartik"
   return (
     <>
 <Chai/>
-<h1>Chai aur react</h1>
+<h1>Chai aur react {username}</h1>
 <p>test para</p>
 </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,42 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp() {
+  return (
+    <div >
+      <h1 >Custom React | Kartik</h1>
+    </div>
+  )
+}
+// const ReactElement = {
+//     type :'a',
+//     props : {
+//      href : 'https://www.google.com',
+//      target : '_blank',
+
+//     },
+//     Children : 'Click Me to visit Google'
+// }
+
+const anothetheElement = (
+  <a href="https://www.google.com" target="_blank">Click Me to visit Google</a>
+)
+
+
+const anotherElement2 = "kartik"
+const reactElement = React.createElement(
+  'a',
+  {
+    href: 'https://www.google.com',
+    target: '_blank'
+  },
+  'Click Me to visit Google',
+  anotherElement2
+)
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+ reactElement
+
 )
